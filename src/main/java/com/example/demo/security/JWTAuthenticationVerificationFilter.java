3b3aa2c71211345b464137f2c8800ca12b7684cf
Anurag <anuragbnrj@gmail.com>
2021-07-29 02:09:06 +0530

    modify security config to return 401 in case of unauthorized requests

--- src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
@@ -1,24 +1,20 @@
 package com.example.demo.security;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import com.auth0.jwt.JWT;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.stereotype.Component;
-
-import com.auth0.jwt.JWT;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
-@Component
 public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilter {
 
     public JWTAuthenticationVerificationFilter(AuthenticationManager authManager) {
